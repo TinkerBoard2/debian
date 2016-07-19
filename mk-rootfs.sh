09e4ece8c37a69151641e78e6d90d96288819b49
Jacob Chen <jacob2.chen@rock-chips.com>
2016-07-19 19:41:59 +0800

    lazy to write

--- mk-rootfs.sh
+++ mk-rootfs.sh
@@ -3,40 +3,54 @@
 # Directory contains the target rootfs
 export TARGET_ROOTFS_DIR="binary"
 
-wget https://releases.linaro.org/debian/images/alip-armhf/latest/linaro-jessie-alip-20160620-25.tar.gz
+if [ ! -e linaro-jessie-alip-*.tar.gz ]; then 
+	wget https://releases.linaro.org/debian/images/alip-armhf/latest/linaro-jessie-alip-20160620-25.tar.gz
+fi
+
 sudo tar -xpf linaro-jessie-alip-*.tar.gz
 
 export TARGET_ROOTFS_DIR="binary"
 sudo cp -rf packages $TARGET_ROOTFS_DIR/
 sudo cp -rf overlay/* $TARGET_ROOTFS_DIR/
+# sudo cp -rf 3288/* $TARGET_ROOTFS_DIR/
 sudo cp /usr/bin/qemu-arm-static $TARGET_ROOTFS_DIR/usr/bin/
 
 sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
+
 # setup rk enviorment
-cat << EOF | sudo chroot binary
+cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
+
+chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+cd /libs/
+bash ./prepare.sh
+
+setcap CAP_SYS_ADMIN+ep /usr/bin/gst-launch-1.0
 
 apt-get update
 
-apt-get install -y libgcrypt20:armhf -t testing
-apt-get install -y xserver-xorg-core -t testing
+#apt-get remove libgbm1:armhf libgles2-mesa:armhf libegl1-mesa:armhf
 
-dpkg --remove --force-depends libegl1-mesa:armhf
-dpkg --remove --force-depends libgbm1:armhf libgles2-mesa:armhf
+apt-get install -y libgcrypt20:armhf  libdbus-1-3:armhf -t testing libdbus-1-dev
+apt-get install -t testing xserver-xorg
+apt-get install -t testing libepoxy0
 
-dpkg -i  /packages/xserver-xorg-core_1.18.21-1_armhf.deb
-dpkg -i  /packages/libmali-rk32881_1.4-1_armhf.deb
-dpkg -i  /packages/libmali-rk3288-dev_1.4-1_armhf.deb
-dpkg -i  /packages/libdrm-rockchip1_2.4.68-2_armhf.deb
-dpkg -i  /packages/libdrm2_2.4.68-2_armhf.deb
+# dpkg -i  /packages/libmali-rk32881_1.4-3_armhf.deb
+# dpkg -i  /packages/libmali-rk3288-dev_1.4-3_armhf.deb
+# dpkg -i /packages/xserver-common_1.18.21-2_all.deb 
+# dpkg -i  /packages/xserver-xorg-core_1.18.21-2_armhf.deb
+# dpkg -i  /packages/libdrm-rockchip1_2.4.68-2_armhf.deb
+# dpkg -i  /packages/libdrm2_2.4.68-2_armhf.deb
 
-rm -rf /var/lib/apt/lists/*
-rm -rf /packages
-rm -rf /libs
+# ln -s /usr/bin/Xorg /usr/bin/X
+
+# rm -rf /var/lib/apt/lists/*
+# rm -rf /packages
+# rm -rf /libs
 
 EOF
 
-
+sudo cp -rf 3288/* $TARGET_ROOTFS_DIR/
 
 sudo umount $TARGET_ROOTFS_DIR/dev
 
--- overlay/libs/prepare.sh
+++ overlay/libs/prepare.sh
@@ -1,9 +1,9 @@
 apt-get update
-apt-get remove chromium.*
-apt-get install gstreamer1.0-vaapi gstreamer1.0-tools libvdpau1 libva1 libva-wayland1 gstreamer1.0-alsa gstreamer1.0-plugins-good gstreamer1.0-plugins-bad libdbus-1-dev alsa-utils
+# apt-get remove -y chromium.*
+apt-get install -y gstreamer1.0-vaapi gstreamer1.0-tools libvdpau1 libva1 libva-wayland1 gstreamer1.0-alsa gstreamer1.0-plugins-good gstreamer1.0-plugins-bad libdbus-1-dev alsa-utils
 
-echo "xhost +" >> /etc/profile
-/etc/init.d/lightdm restart &
+# echo "xhost +" >> /etc/profile
+# /etc/init.d/lightdm restart &
 
 rm `find /usr/lib/ -name "*vaapi*"`
 rm /etc/init/tiny*
@@ -15,9 +15,7 @@ cp librkdec-h264d.so /usr/lib/arm-linux-gnueabihf/
 cp libgstvaapi.so libgstvaapi_parse.so /usr/lib/arm-linux-gnueabihf/gstreamer-1.0/
 cp -r gstvaapi/* /usr/lib/arm-linux-gnueabihf/
 
-cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
 
-cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
 cp librkenc-h264e.so /usr/lib/arm-linux-gnueabihf/
 
 cp test.mp4 /usr/local/
@@ -25,3 +23,8 @@ cp test_* /usr/local/bin/
 cp statistics.sh /usr/local/bin/
 
 cp -r alsa_init/* /usr/share/alsa/init/
+
+
+#cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
+
+#cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
