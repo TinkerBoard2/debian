0d5d4fbdf4aa0cb66983c1bde2a95a6f5b7801fb
Jacob Chen <jacob2.chen@rock-chips.com>
2016-07-20 13:13:06 +0800

    lazy!!!!!!!!!!!!!!!!!!!
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- mk-rootfs.sh
+++ mk-rootfs.sh
@@ -4,53 +4,60 @@
 export TARGET_ROOTFS_DIR="binary"
 
 if [ ! -e linaro-jessie-alip-*.tar.gz ]; then 
+	echo Download linaro rootfs
 	wget https://releases.linaro.org/debian/images/alip-armhf/latest/linaro-jessie-alip-20160620-25.tar.gz
 fi
 
+echo Extract image
 sudo tar -xpf linaro-jessie-alip-*.tar.gz
 
-export TARGET_ROOTFS_DIR="binary"
+echo Copy overlay to rootfs
 sudo cp -rf packages $TARGET_ROOTFS_DIR/
 sudo cp -rf overlay/* $TARGET_ROOTFS_DIR/
 # sudo cp -rf 3288/* $TARGET_ROOTFS_DIR/
+
+echo Change root.....................
 sudo cp /usr/bin/qemu-arm-static $TARGET_ROOTFS_DIR/usr/bin/
-
 sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
-
-
 # setup rk enviorment
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
-
 chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper
-cd /libs/
-bash ./prepare.sh
-
-setcap CAP_SYS_ADMIN+ep /usr/bin/gst-launch-1.0
-
 apt-get update
-
-#apt-get remove libgbm1:armhf libgles2-mesa:armhf libegl1-mesa:armhf
-
 apt-get install -y libgcrypt20:armhf  libdbus-1-3:armhf -t testing libdbus-1-dev
-apt-get install -t testing xserver-xorg
-apt-get install -t testing libepoxy0
+apt-get remove -y xserver-xorg
+apt-get install -y -t testing xserver-xorg-core 
+apt-get install -y -t testing xserver-xorg-input-evdev
+apt-get install -y -t testing libepoxy0
+dpkg -i  /packages/libmali-rk32881_1.4-3_armhf.deb
+dpkg -i  /packages/libmali-rk3288-dev_1.4-3_armhf.deb
+dpkg -i  /packages/xserver-common_1.18.21-2_all.deb 
+dpkg -i  /packages/xserver-xorg-core_1.18.21-2_armhf.deb
+dpkg -i  /packages/libdrm-rockchip1_2.4.68-2_armhf.deb
+dpkg -i  /packages/libdrm2_2.4.68-2_armhf.deb
+apt-get install -y gstreamer1.0-vaapi gstreamer1.0-tools libvdpau1 libva1 \
+	 libva-wayland1 gstreamer1.0-alsa gstreamer1.0-plugins-good 	\
+	 gstreamer1.0-plugins-bad libdbus-1-dev alsa-utils vdpau-va-driver
 
-# dpkg -i  /packages/libmali-rk32881_1.4-3_armhf.deb
-# dpkg -i  /packages/libmali-rk3288-dev_1.4-3_armhf.deb
-# dpkg -i /packages/xserver-common_1.18.21-2_all.deb 
-# dpkg -i  /packages/xserver-xorg-core_1.18.21-2_armhf.deb
-# dpkg -i  /packages/libdrm-rockchip1_2.4.68-2_armhf.deb
-# dpkg -i  /packages/libdrm2_2.4.68-2_armhf.deb
+dpkg -i  /packages/libva-rockchip1_0.20-1_armhf.deb
+dpkg -i  /packages/rockchip-vdpau-drivers_0.20-1_armhf.deb
 
-# ln -s /usr/bin/Xorg /usr/bin/X
+# Prepare VDPAU VAAPI enviorment
+cp /libs/test.mp4 /usr/local/
+cp /libs/test_* /usr/local/bin/
+cp /libs/statistics.sh /usr/local/bin/
+cp /libs/vdpau_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/vdpau_drv_video.so
+cp /libs/libgstvaapi.so libgstvaapi_parse.so /usr/lib/arm-linux-gnueabihf/gstreamer-1.0/
+cp -r /libs/gstvaapi/* /usr/lib/arm-linux-gnueabihf/
 
 # rm -rf /var/lib/apt/lists/*
 # rm -rf /packages
 # rm -rf /libs
 
-EOF
+ln -s /usr/bin/Xorg /usr/bin/X
+apt-get install bash-completion
+setcap CAP_SYS_ADMIN+ep /usr/bin/gst-launch-1.0
 
-sudo cp -rf 3288/* $TARGET_ROOTFS_DIR/
+EOF
 
 sudo umount $TARGET_ROOTFS_DIR/dev
 
--- overlay/libs/prepare.sh
+++ overlay/libs/prepare.sh
@@ -1,20 +1,26 @@
 apt-get update
 # apt-get remove -y chromium.*
-apt-get install -y gstreamer1.0-vaapi gstreamer1.0-tools libvdpau1 libva1 libva-wayland1 gstreamer1.0-alsa gstreamer1.0-plugins-good gstreamer1.0-plugins-bad libdbus-1-dev alsa-utils
+apt-get install -y gstreamer1.0-vaapi gstreamer1.0-tools libvdpau1 libva1 \
+	 libva-wayland1 gstreamer1.0-alsa gstreamer1.0-plugins-good 	\
+	 gstreamer1.0-plugins-bad libdbus-1-dev alsa-utils vdpau-va-driver
 
 # echo "xhost +" >> /etc/profile
 # /etc/init.d/lightdm restart &
 
-rm `find /usr/lib/ -name "*vaapi*"`
-rm /etc/init/tiny*
-rm /usr/sbin/tiny*
-
-cp libvdpau_rockchip.so.1 /usr/lib/arm-linux-gnueabihf/vdpau/libvdpau_rockchip.so.1
-cp vdpau_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/vdpau_drv_video.so
+# rm `find /usr/lib/ -name "*vaapi*"`
+# rm /etc/init/tiny*
+# rm /usr/sbin/tiny*
 cp librkdec-h264d.so /usr/lib/arm-linux-gnueabihf/
+cp libvdpau_rockchip.so.1 /usr/lib/arm-linux-gnueabihf/vdpau/libvdpau_rockchip.so.1
+
+cp vdpau_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/vdpau_drv_video.so
+
 cp libgstvaapi.so libgstvaapi_parse.so /usr/lib/arm-linux-gnueabihf/gstreamer-1.0/
 cp -r gstvaapi/* /usr/lib/arm-linux-gnueabihf/
 
+cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
+
+cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
 
 cp librkenc-h264e.so /usr/lib/arm-linux-gnueabihf/
 
@@ -25,6 +31,3 @@ cp statistics.sh /usr/local/bin/
 cp -r alsa_init/* /usr/share/alsa/init/
 
 
-#cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
-
-#cp rockchip_drv_video.so /usr/lib/arm-linux-gnueabihf/dri/
