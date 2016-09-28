2ea3d11d391c877c1061b606ddbf6f9084f2dda8
Jacob Chen <jacob2.chen@rock-chips.com>
2016-09-28 18:29:59 +0800

    script: change to linaro stretch
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- mk-rootfs.sh
+++ mk-rootfs.sh
@@ -1,58 +1,69 @@
 #!/bin/bash -e
 
 # Directory contains the target rootfs
-export TARGET_ROOTFS_DIR="binary"
-export ARCH="armhf"
-
+TARGET_ROOTFS_DIR="binary"
+ARCH="armhf"
 # none or debug(adb,ssh) or demo(more apps)
-export VERSION="demo"
+VERSION="debug"
 
-if [ ! -e linaro-jessie-alip-*.tar.gz ]; then 
+if [ ! -e linaro-stretch-alip-*.tar.gz ]; then 
 	echo Download linaro rootfs
-	wget https://releases.linaro.org/debian/images/alip-armhf/16.07/linaro-jessie-alip-20160722-27.tar.gz
+	echo "linaro didn't have a official stretch release, so we can't download it."
+	echo "For the moment, you'd  better download image directly from our google drive."
+	exit
+	# wget http://snapshots.linaro.org/debian/images/stretch/blend-armhf/1/linaro-stretch-alip-20160921-1.tar.gz
 fi
 
-echo "Extract image"
-sudo tar -xpf linaro-jessie-alip-*.tar.gz
+echo -e "\033[36m Extract image \033[0m"
+sudo tar -xpf linaro-stretch-alip-*.tar.gz
 
-echo "Copy overlay to rootfs"
+echo -e "\033[36m Copy overlay to rootfs \033[0m"
 sudo mkdir -p $TARGET_ROOTFS_DIR/packages
 sudo cp -rf packages/$ARCH/* $TARGET_ROOTFS_DIR/packages
 sudo cp -rf overlay/* $TARGET_ROOTFS_DIR/
+sudo cp -rf overlay-firmware/* $TARGET_ROOTFS_DIR/
+if [ "$VERSION" == "debug" ] || [ "$VERSION" == "demo" ] ; then
+	sudo cp -rf overlay-debug/* $TARGET_ROOTFS_DIR/
+fi
+if [ "$VERSION" == "demo" ] ; then
+	sudo cp -rf overlay-demo/* $TARGET_ROOTFS_DIR/
+fi
 
-echo "Change root....................."
+echo -e "\033[36m Change root.....................\033[0m"
 sudo cp /usr/bin/qemu-arm-static $TARGET_ROOTFS_DIR/usr/bin/
 sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
+
 chmod o+x /usr/lib/dbus-1.0/dbus-daemon-launch-helper
 apt-get update
 
-#---------------Xserver-------------- 
-apt-get install -y libgcrypt20  libdbus-1-3  libdbus-1-dev -t testing
-apt-get remove -y xserver-xorg
-apt-get install -y -t testing xserver-xorg-core 
-apt-get install -y -t testing xserver-xorg-input-evdev
-apt-get install -y -t testing libepoxy0
+apt-get install -y systemd-sysv
 
+#---------------Xserver-------------- 
+echo -e "\033[36m Setup Xserver.................... \033[0m"
 dpkg -i  /packages/xserver-common_*_all.deb 
 dpkg -i  /packages/xserver-xorg-core_*_armhf.deb
+apt-get  remove xserver-xorg-video-fbdev  xserver-xorg
+apt-get install -f -y
 
-#---------------LIBDRM-------------- 
+#---------------libdrm-------------- 
+echo -e "\033[36m Setup libdrm.................... \033[0m"
 dpkg -i  /packages/libdrm/*
+apt-get install -f -y
 
 #---------------Video-Vaapi-------------- 
-apt-get install -y -t testing gstreamer1.0-vaapi gstreamer1.0-tools libvdpau1 libva1 \
+echo -e "\033[36m Setup vaapi.................... \033[0m"
+apt-get install -y gstreamer1.0-vaapi gstreamer1.0-tools libvdpau1 libva1 \
 	 libva-wayland1 gstreamer1.0-alsa gstreamer1.0-plugins-good 	\
 	 gstreamer1.0-plugins-bad alsa-utils vdpau-va-driver gstreamer1.0-x
 dpkg -i  /packages/video/gstreamer1.0-vaapi_*_armhf.deb
 dpkg -i  /packages/video/libva-rockchip*_armhf.deb
-# dpkg -i  /packages/video/rockchip-vdpau-drivers_*_armhf.deb
 apt-get install -f -y
 
 #---------------Debug-------------- 
 if [ "$VERSION" == "debug" ] || [ "$VERSION" == "demo" ] ; then
-	apt-get install -y sshfs openssh-server -t testing 
+	apt-get install -y sshfs openssh-server bash-completion 
 fi
 
 #---------------Custom Script-------------- 
@@ -61,29 +72,17 @@ ln -s /etc/init.d/rockchip.sh /etc/rcS.d/S11rockchip.sh
 
 apt-get remove -y chromium
 apt-get install -y iceweasel
-apt-get install -f
+apt-get install -f -y
 
 #---------------Demo-------------- 
 if [ "$VERSION" == "demo" ] ; then
-	apt-get install -y xserver-xorg-input-synaptics -t testing
-	apt-get install -y cheese fswebcam bash-completion 
-	# apt-get install -y libreoffice -t testing
+	apt-get install -y xserver-xorg-input-synaptics
+	apt-get install -y cheese fswebcam
 fi
 
 #---------------Clean-------------- 
 rm -rf /var/lib/apt/lists/*
-rm -rf /libs
 
 EOF
-
-sudo cp -rf overlay-firmware/* $TARGET_ROOTFS_DIR/
-
-if [ "$VERSION" == "debug" ] || [ "$VERSION" == "demo" ] ; then
-	sudo cp -rf overlay-debug/* $TARGET_ROOTFS_DIR/
-fi
-if [ "$VERSION" == "demo" ] ; then
-	sudo cp -rf overlay-demo/* $TARGET_ROOTFS_DIR/
-fi
-
 
 sudo umount $TARGET_ROOTFS_DIR/dev
