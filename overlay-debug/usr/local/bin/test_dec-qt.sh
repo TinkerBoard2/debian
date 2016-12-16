727c51c86448b6708e81a830215d95345e636c0e
Jacob Chen <jacob2.chen@rock-chips.com>
2016-12-16 16:04:45 +0800

    overlay-debug: update new video decode script
    vaapi is deprecated
    
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

---
+++ overlay-debug/usr/local/bin/test_dec-qt.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export DISPLAY=:0.0 
+#export GST_DEBUG=*:5
+#export GST_DEBUG_FILE=/tmp/2.txt
+
+su linaro -c "DISPLAY=:0.0 /usr/lib/arm-linux-gnueabihf/qt5/examples/multimediawidgets/playe
+r/player /usr/local/test.mp4 "
