f301575a3abbdfbd183bbbab3884acdcb6096714
Jacob Chen <jacob2.chen@rock-chips.com>
2017-04-25 12:38:08 +0800

    video: fix test enc
    Change-Id: I899e436fcf11950c53bd42b51a2fb818d77ab6b6
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- overlay-debug/usr/local/bin/test_enc.sh
+++ overlay-debug/usr/local/bin/test_enc.sh
@@ -3,19 +3,18 @@ export DISPLAY=:0.0
 #export GST_DEBUG=*:5
 #export GST_DEBUG_FILE=/tmp/2.txt
 
-echo "message: decoding raw video!"
+echo "message: downloading raw sample!"
 
-#dump raw video
-gst-launch-1.0 filesrc location=/usr/local/test.mp4 ! \
-    qtdemux ! queue ! h264parse ! mppvideodec ! queue ! \
-    filesink location=/usr/local/output.yuv
+[ ! -e /usr/local/sample.yuv ] && sudo apt-get update && sudo apt-get install dtrx \
+    && wget http://trace.eas.asu.edu/yuv/bridge-close/bridge-close_qcif.7z \
+    && dtrx -n bridge-close_qcif.7z && mv bridge-close_qcif/bridge-close_qcif.yuv /usr/local/sample.yuv
 
 echo "message: encoding ....."
 
-gst-launch-1.0 -v filesrc location=/usr/local/output.yuv ! \
- videoparse format=nv12 width=1920 height=1080 framerate=24 ! mpph264enc ! \
-h264parse ! mp4mux ! filesink location=/usr/local/output.mp4
+gst-launch-1.0 -v filesrc location=/usr/local/sample.yuv  ! \
+ videoparse format=yv12 width=176 height=144 framerate=24 ! videoconvert ! mpph264enc ! \
+h264parse ! filesink location=/usr/local/test.h264
 
 echo "message: playing encoded video!"
 
-su linaro -c "gst-launch-1.0 uridecodebin uri=file:///usr/local/output.mp4  ! rkximagesink" 
+su linaro -c "gst-launch-1.0 uridecodebin uri=file:///usr/local/test.h264  ! rkximagesink"
