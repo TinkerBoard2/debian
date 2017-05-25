7a850c758145161a5b739ecd0cf5af04353cdcb2
陈豪 | Jacob Chen <jacob-chen@iotwrt.com>
2017-05-25 13:46:25 +0800

    Update test_enc.sh

--- overlay-debug/usr/local/bin/test_enc.sh
+++ overlay-debug/usr/local/bin/test_enc.sh
@@ -3,18 +3,10 @@ export DISPLAY=:0.0
 #export GST_DEBUG=*:5
 #export GST_DEBUG_FILE=/tmp/2.txt
 
-echo "message: downloading raw sample!"
+echo "message: output to /home/linaro/2k.h264!"
 
-[ ! -e /usr/local/sample.yuv ] && sudo apt-get update && sudo apt-get install dtrx \
-    && wget http://trace.eas.asu.edu/yuv/bridge-close/bridge-close_qcif.7z \
-    && dtrx -n bridge-close_qcif.7z && mv bridge-close_qcif/bridge-close_qcif.yuv /usr/local/sample.yuv
-
-echo "message: encoding ....."
-
-gst-launch-1.0 -v filesrc location=/usr/local/sample.yuv  ! \
- videoparse format=yv12 width=176 height=144 framerate=24 ! videoconvert ! mpph264enc ! \
-h264parse ! filesink location=/usr/local/test.h264
+gst-launch-1.0 videotestsrc ! video/x-raw,format=NV12,width=1920,height= 1080,framerate=30/1 ! queue ! mpph264enc ! filesink location=/home/linaro/2k.h264
 
 echo "message: playing encoded video!"
 
-su linaro -c "gst-launch-1.0 uridecodebin uri=file:///usr/local/test.h264  ! rkximagesink"
+su linaro -c "gst-launch-1.0 uridecodebin uri=file:///home/linaro/2k.h264  ! rkximagesink"
