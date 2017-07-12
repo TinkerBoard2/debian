7691dfadc848e9eafd40c8008dd677bfb5bd27c3
Jacob Chen <jacob2.chen@rock-chips.com>
2017-07-12 11:58:19 +0800

    overlay: debug: use dmabuf for camera
    To get zero-copy
    
    Change-Id: I6740a5caee157561db1b775115e011288d5b4d89
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- overlay-debug/usr/local/bin/test_camera.sh
+++ overlay-debug/usr/local/bin/test_camera.sh
@@ -5,5 +5,5 @@ export DISPLAY=:0.0
 #export GST_DEBUG_FILE=/tmp/2.txt
 
 su linaro -c " \
-    gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! video/x-raw,format=NV12,width=640,height=480  ! rkximagesink \
+    gst-launch-1.0 v4l2src device=/dev/video0 io-mode=4 ! videoconvert ! video/x-raw,format=NV12,width=640,height=480  ! rkximagesink \
 " 
