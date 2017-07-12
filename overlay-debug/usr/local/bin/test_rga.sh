3883ef26dc24d02a5b81321560dbd10ffc1e7162
Jacob Chen <jacob2.chen@rock-chips.com>
2017-07-12 11:11:21 +0800

    overlay: debug: add gstreamer camera scirpt
    cif10_isp driver support videobuf2 now, which means it work with gstreamer.
    
    Change-Id: I051cc930c96bc398ba475913eb1b317cb175bd22
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- overlay-debug/usr/local/bin/test_rga.sh
+++ overlay-debug/usr/local/bin/test_rga.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-sudo service lightdm stop
+export DISPLAY=:0.0 
+#export GST_DEBUG=*:5
+#export GST_DEBUG_FILE=/tmp/2.txt
 
-sudo gst-launch-1.0 -v videotestsrc ! "video/x-raw,format=BGRA, width=1920,height=1080,framerate=30/1" ! \ 
+su linaro -c " \
+    gst-launch-1.0 -v videotestsrc ! "video/x-raw,format=BGRA, width=1920,height=1080,framerate=30/1" ! \ 
     v4l2video0convert output-io-mode=dmabuf capture-io-mode=dmabuf ! \
-    "video/x-raw,format=NV12, width=640,height=720,framerate=30/1, pixel-aspect-ratio=8/9" ! kmssink
+    "video/x-raw,format=NV12, width=640,height=720,framerate=30/1, pixel-aspect-ratio=8/9" ! rkximagesink \
+" 
