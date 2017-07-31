d50432ea0a2d5a5282d42971f58e7393e307684d
Jacob Chen <jacob2.chen@rock-chips.com>
2017-07-31 15:38:20 +0800

    overlay: debug:  test_rga: fix double quotes
    Change-Id: I3c7470bad3da8c97351bb29f78d702367c1fe987
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- overlay-debug/usr/local/bin/test_rga.sh
+++ overlay-debug/usr/local/bin/test_rga.sh
@@ -4,8 +4,9 @@ export DISPLAY=:0.0
 #export GST_DEBUG=*:5
 #export GST_DEBUG_FILE=/tmp/2.txt
 
-su linaro -c " \
+su linaro -c ' \
     gst-launch-1.0 -v videotestsrc ! "video/x-raw,format=BGRA, width=1920,height=1080,framerate=30/1" ! \ 
     v4l2video0convert output-io-mode=dmabuf capture-io-mode=dmabuf ! \
+
     "video/x-raw,format=NV12, width=640,height=720,framerate=30/1, pixel-aspect-ratio=8/9" ! rkximagesink \
-" 
+'
