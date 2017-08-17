2784b6be44ea7c37343b471b9968bde6189e7476
Jacob Chen <jacob-chen@iotwrt.com>
2017-08-17 12:57:48 +0800

    overlay: debug: update camera preview scripts for uvc/mipi csi
    uvc camera should use mjpeg formats.
    MIPI camera should use nv12 formats.
    
    Change-Id: Ib87b8a9d77f32b913fb3cd7877ce4ba8248c3fd2
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- overlay-debug/usr/local/bin/test_camera.sh
+++ overlay-debug/usr/local/bin/test_camera.sh
@@ -4,6 +4,12 @@ export DISPLAY=:0.0
 #export GST_DEBUG=*:5
 #export GST_DEBUG_FILE=/tmp/2.txt
 
+echo "Start MIPI CSI Camera Preview!"
+
 su linaro -c " \
     gst-launch-1.0 v4l2src device=/dev/video0 io-mode=4 ! videoconvert ! video/x-raw,format=NV12,width=640,height=480  ! rkximagesink \
 " 
+
+# If you are using uvc camera which don't support output NV12,
+# then you have to use RGA to convert color formats.
+# See test_rga.sh.
