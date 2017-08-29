9a152c8a31abedc43c2a982a4cbc240dd011260d
Jacob Chen <jacob-chen@iotwrt.com>
2017-08-29 09:00:09 +0800

    debug: isp: remind to remove libv4l-mplane.so
    Change-Id: I066a849b8efb1a364b8b506f6b4e0b87fabf22cc
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- overlay-debug/usr/local/bin/test_camera.sh
+++ overlay-debug/usr/local/bin/test_camera.sh
@@ -5,6 +5,9 @@ export DISPLAY=:0.0
 #export GST_DEBUG_FILE=/tmp/2.txt
 
 echo "Start MIPI CSI Camera Preview!"
+echo "Please make sure libv4l-mplane are removed."
+
+#rm /usr/lib/arm-linux-gnueabihf/libv4l/plugins/libv4l-mplane.so
 
 su linaro -c " \
     gst-launch-1.0 v4l2src device=/dev/video0 io-mode=4 ! videoconvert ! video/x-raw,format=NV12,width=640,height=480  ! rkximagesink \
