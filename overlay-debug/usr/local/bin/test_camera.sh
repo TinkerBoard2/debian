253dfb6388faa9a3e9b381c73b037031e7ebf8b1
Jacob Chen <jacob-chen@iotwrt.com>
2017-09-14 12:07:27 +0800

    overlay: debug: update isp test script
    use ispsrc
    
    Change-Id: I6f6ca7053c7931b4e8047a303da889d8c75ec030
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- overlay-debug/usr/local/bin/test_camera.sh
+++ overlay-debug/usr/local/bin/test_camera.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 export DISPLAY=:0.0 
-#export GST_DEBUG=*:5
+#export GST_DEBUG=ispsrc:5
 #export GST_DEBUG_FILE=/tmp/2.txt
 
 echo "Start MIPI CSI Camera Preview!"
-echo "Please make sure libv4l-mplane are removed."
 
-#rm /usr/lib/arm-linux-gnueabihf/libv4l/plugins/libv4l-mplane.so
+echo 1 > /sys/module/video_rkisp1/parameters/rkisp1_debug
 
 su linaro -c " \
-    gst-launch-1.0 v4l2src device=/dev/video0 io-mode=4 ! videoconvert ! video/x-raw,format=NV12,width=640,height=480  ! rkximagesink \
+    gst-launch-1.0 ispsrc device=/dev/video0 io-mode=4 ! videoconvert ! video/x-raw,format=NV12,width=640,height=480  ! rkximagesink \
 " 
-
-# If you are using uvc camera which don't support output NV12,
-# then you have to use RGA to convert color formats.
-# See test_rga.sh.
