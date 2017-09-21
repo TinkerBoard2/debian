b4663417ac7f54bcc530a26da852ab3b58cbd37b
Jacob Chen <jacob-chen@iotwrt.com>
2017-09-21 20:57:36 +0800

    overlay: test: add dmabuf-import case
    it have better performance
    need kernel commit "quick fix for dmabuf"
    
    Change-Id: I11585640d5f680fae062b9272132a49aee81d746
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- overlay-debug/usr/local/bin/test_rga.sh
+++ overlay-debug/usr/local/bin/test_rga.sh
@@ -10,3 +10,33 @@ su linaro -c ' \
      output-io-mode=dmabuf capture-io-mode=dmabuf ! \
     "video/x-raw,format=NV12, width=640,height=640x360,framerate=30/1" ! rkximagesink \
 '
+
+
+: '
+Video convert pipeline:
+
+gst-launch-1.0  filesrc location=/usr/local/test.mp4 ! qtdemux ! queue ! h264parse ! mppvideodec  ! queue ! \
+   rgaconvert output-io-mode=dmabuf-import capture-io-mode=dmabuf ! \
+   "video/x-raw,format=NV12, width=640,height=720,pixel-aspect-ratio=8/9"  ! rkximagesink
+'
+
+: '
+WorkAround kernel patch for video convert pipeline:
+
+   driver/media/platform/rockchip-rga/rga-hw.c
+   @@ -73,10 +73,16 @@ rga_get_addr_offset(struct rga_frame *frm, unsigned int x, unsigned int y,
+ 	lt->y_off = y * frm->stride + x * pixel_width;
+ 	lt->u_off =
+ 		frm->width * frm->height + (y / y_div) * uv_stride + x / x_div;
+ 	lt->v_off = lt->u_off + frm->width * frm->height / uv_factor;
+ 
++	/* workaround for ver_stride from vpu.... */
++	if(frm->fmt->fourcc == V4L2_PIX_FMT_NV12);
++		lt->u_off = DIV_ROUND_UP(frm->width, 32) *
++			DIV_ROUND_UP(frm->height, 32) +
++			(y / y_div) * uv_stride + x / x_div;
++
+ 	lb->y_off = lt->y_off + (h - 1) * frm->stride;
+ 	lb->u_off = lt->u_off + (h / y_div - 1) * uv_stride;
+ 	lb->v_off = lt->v_off + (h / y_div - 1) * uv_stride;
+'
