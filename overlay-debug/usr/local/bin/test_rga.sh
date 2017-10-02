c50451e0b0adfd3b71ba1fc5e2c795b917dfdfcf
Jacob Chen <jacob-chen@iotwrt.com>
2017-10-02 21:55:43 +0800

    overlay-debug: add execute permission for scripts
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- overlay-debug/usr/local/bin/test_rga.sh
+++ overlay-debug/usr/local/bin/test_rga.sh
@@ -8,7 +8,7 @@ su linaro -c ' \
     gst-launch-1.0 -v videotestsrc ! "video/x-raw,format=BGRA, width=1920,height=1080,framerate=30/1" ! \
     rgaconvert hflip=false vflip=false rotation=90 input-crop=0x0x1920x1080 output-crop=0x0x640x360 \
      output-io-mode=dmabuf capture-io-mode=dmabuf ! \
-    "video/x-raw,format=NV12, width=640,height=640x360,framerate=30/1" ! rkximagesink \
+    "video/x-raw,format=NV12, width=640,height=360,framerate=30/1" ! rkximagesink \
 '
 
 : '
