71a56927d86c49f0519051453779d2b08d067aad
Jacob Chen <jacob2.chen@rock-chips.com>
2018-01-17 17:59:55 +0800

    overlay: debug: test_dec: set plane-id for rk3036 to avoid wrong use of plane
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- overlay-debug/usr/local/bin/test_dec-gst.sh
+++ overlay-debug/usr/local/bin/test_dec-gst.sh
@@ -8,7 +8,7 @@ case "$1" in
 	rk3036) ###TODO:Display bug, since rk3036 just two overlay.
 	sudo service lightdm stop
 	sleep 2
-	su linaro -c "gst-launch-1.0 playbin uri=file:///usr/local/test.mp4"
+	su linaro -c "gst-launch-1.0 uridecodebin uri=file:///usr/local/test.mp4 ! kmssink plane-id=52"
 	sudo service lightdm start
 	;;
 *)
