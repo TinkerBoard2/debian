be0167010e388f624d30a800681658838aff838f
Jacob Chen <jacob2.chen@rock-chips.com>
2018-01-17 18:03:04 +0800

    ooverlay: debug: warning that qtmultimedia with x11 is not support for rk3036
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- overlay-debug/usr/local/bin/test_dec-qt.sh
+++ overlay-debug/usr/local/bin/test_dec-qt.sh
@@ -8,8 +8,7 @@ export DISPLAY=:0.0
 
 case "$1" in
 	rk3036) ###default the rk3036 use kmssink.
-	export QT_GSTREAMER_WIDGET_VIDEOSINK=kmssink
-	su linaro -c "DISPLAY=:0.0 /usr/lib/arm-linux-gnueabihf/qt5/examples/multimediawidgets/player/player /usr/local/test.mp4 "
+	echo "rk3036: don't support qtmultimedia with X11, please use https://github.com/rockchip-linux/meta-rockchip-extra/tree/master/demo/meta-player-qt."
 	;;
 	*)
 	export QT_GSTREAMER_WIDGET_VIDEOSINK=rkximagesink
