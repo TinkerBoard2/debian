4c53766147a12171bd36e1045344fd873253834c
Caesar Wang <wxt@rock-chips.com>
2018-08-16 16:50:12 +0800

    adb: add the depend lib libcrypto.so.1.0.0 for adb 64bit
    Change-Id: I6fab9d4bf95f5aa9dde1a1cf48f2a94db6955ff7
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- mk-rootfs-stretch-arm64.sh
+++ mk-rootfs-stretch-arm64.sh
@@ -39,6 +39,7 @@ if [ "$ARCH" == "arm64"  ]; then
     sudo cp -rf overlay-debug/usr/local/share/adb/adbd-64 $TARGET_ROOTFS_DIR/usr/local/sbin/adbd
     sudo cp -rf overlay-debug/usr/local/share/adb/S60adbd $TARGET_ROOTFS_DIR/usr/local/sbin
     sudo cp -rf overlay-debug/usr/local/share/adb/libcutils.so $TARGET_ROOTFS_DIR/usr/lib
+    sudo cp -rf overlay-debug/usr/local/share/adb/libcrypto.so.1.0.0 $TARGET_ROOTFS_DIR/usr/lib
 fi
 
 if [ "$VERSION" == "jenkins" ]; then
