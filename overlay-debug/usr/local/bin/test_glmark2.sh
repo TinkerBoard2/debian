d024f647399c83635517b14189a48edb94a61357
Caesar Wang <wxt@rock-chips.com>
2019-03-26 11:41:57 +0800

    overlay-debug: update test_glmark2.sh
    Tested on rk3399pro evb board
    
    full-screen:
    =======================================================
                                      glmark2 Score: 63
    =======================================================
    
    off-screen:
    =======================================================
                                      glmark2 Score: 818
    =======================================================
    
    Change-Id: I082adcc10f58c794c3f6874463fcea6e288494f7
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-debug/usr/local/bin/test_glmark2.sh
+++ overlay-debug/usr/local/bin/test_glmark2.sh
@@ -3,6 +3,15 @@
 echo performance | tee $(find /sys/ -name *governor)
 
 echo "running glmark2 for testing GPU!!"
+echo "please use the 'test_glmark2.sh fullscreen' or 'test_glamrk2.sh offscreen'"
 
-su linaro -c "DISPLAY=:0.0 /usr/local/bin/glmark2-es2 --fullscreen"
+case "$1" in
 
+fullscreen)
+    su linaro -c "DISPLAY=:0.0  taskset -c 4-5 /usr/local/bin/glmark2-es2 --fullscreen"
+	;;
+offscreen)
+    su linaro -c "DISPLAY=:0.0  taskset -c 4-5 /usr/local/bin/glmark2-es2 --off-screen"
+	;;
+esac
+shift
