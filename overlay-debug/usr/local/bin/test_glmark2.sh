bf9c0e8e1e236f1e3de701910e582a939edd1287
Caesar Wang <wxt@rock-chips.com>
2019-05-06 10:23:41 +0800

    overlay-debug: improve the glmark2 for all socs supported
    you can run "test_glmark2.sh offscreen" or "test_glmark2.sh fullscreen"
    for testing, also run "test_glmark2_fullscreen.sh" or
    "test_glmark2_offscreen.sh" to test the glmark2.
    
    Change-Id: I59d57d95735908f09156c4db1d385c8e65f9317f
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-debug/usr/local/bin/test_glmark2.sh
+++ overlay-debug/usr/local/bin/test_glmark2.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-echo performance | tee $(find /sys/ -name *governor)
-
 echo "running glmark2 for testing GPU!!"
 echo "please use the 'test_glmark2.sh fullscreen' or 'test_glamrk2.sh offscreen'"
 
 case "$1" in
 
 fullscreen)
-    su linaro -c "DISPLAY=:0.0  taskset -c 4-5 /usr/local/bin/glmark2-es2 --fullscreen"
+	/usr/local/bin/test_glmark2_fullscreen.sh
 	;;
 offscreen)
-    su linaro -c "DISPLAY=:0.0  taskset -c 4-5 /usr/local/bin/glmark2-es2 --off-screen"
+	/usr/local/bin/test_glmark2_offscreen.sh
 	;;
 esac
 shift
