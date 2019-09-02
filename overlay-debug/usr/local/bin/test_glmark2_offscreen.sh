1a9f7740772009498d04f72722b5ccb7ab44892d
Caesar Wang <wxt@rock-chips.com>
2019-09-02 10:13:10 +0800

    overlay-debug: fixes the glmark2_offscreen
    Change-Id: Idfa433ac28cb65eca10ce3b8d6316544ef5fef20
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-debug/usr/local/bin/test_glmark2_offscreen.sh
+++ overlay-debug/usr/local/bin/test_glmark2_offscreen.sh
@@ -27,7 +27,7 @@ else
 fi
 }
 
-COMPATIBLE=$(tr -d '\0' /proc/device-tree/compatible)
+COMPATIBLE=$(cat /proc/device-tree/compatible)
 if [[ $(expr $COMPATIBLE : ".*rk3288") -ne 0 ]]; then
     CHIPNAME="rk3288"
 elif [[ $(expr $COMPATIBLE : ".*rk3308") -ne 0 ]]; then
