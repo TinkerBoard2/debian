5d0d7ba32a596e4926d158c1ff1c5f12f2f60100
Shunqian Zheng <zhengsq@rock-chips.com>
2019-09-11 09:29:23 +0000

    overlay: camera: use v4l2src instead of rkv4l2src
    Change-Id: I4317a52093738e42c3993b820163061780be7d65
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>

--- overlay-debug/usr/local/bin/test_camera-rkisp1.sh
+++ overlay-debug/usr/local/bin/test_camera-rkisp1.sh
@@ -6,4 +6,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/aarch64-linux-gnu/gstreamer-1.0
 #export GST_DEBUG_FILE=/tmp/2.txt
 echo "Start RKISP Camera Preview!"
 su linaro -c " \
-	gst-launch-1.0 rkv4l2src device=/dev/video0 ! video/x-raw,format=NV12,width=1920,height=1080, framerate=30/1 ! videoconvert ! autovideosink"
+	gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,format=NV12,width=1920,height=1080, framerate=30/1 ! videoconvert ! autovideosink"
