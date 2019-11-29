ca9e1bedf6fcebdd34693564982018f30b035d26
Caesar Wang <wxt@rock-chips.com>
2019-11-29 09:27:45 +0800

    overlay: update rknpu_pcie to v1.3.0 for rk3399pro
     1. update galcore.ko (6.4.0.227915 030570c6)
               rk1808 base on kernel commit id:
                   d81e5390d0921d7fb019f3a45ab08d5e0e1d2fb0
               rk1808 fedora:
                   b75e99f54361dd71fe7896919076692ae97b7585
               rk3399pro/rk3399pro pcie:
                   577aa02a6d309d0697db079c673baf0b815f5d53
    2. update rknn_server to 1.3.0 (c4f8c233)
    3. update librknn_runtime to 1.3.0 (0e1d4ea6)
    4. New features:
        1) Add multi-channels-mean support
        2) New op: argmax argmin gru grucell log_softmax
    
    Change-Id: If3c8cd30173d2d8f59d62fbfb9868ad0f199f655
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-firmware/usr/bin/npu-image.sh
+++ overlay-firmware/usr/bin/npu-image.sh
@@ -1,7 +1,13 @@
-
-cd /usr/share/npu_fw/
-
-npu_upgrade MiniLoaderAll.bin uboot.img trust.img boot.img
+if [ -e "/sys/devices/platform/f8000000.pcie/pcie_reset_ep" ] ;
+then
+    echo "upgrading npu pcie image......\n"
+    cd /usr/share/npu_fw_pcie/
+    npu_upgrade_pcie MiniLoaderAll.bin uboot.img trust.img boot.img
+else
+    echo "upgrading npu image......\n"
+    cd /usr/share/npu_fw/
+    npu_upgrade MiniLoaderAll.bin uboot.img trust.img boot.img
+fi
 
 sleep 1
 
