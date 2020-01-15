e8f1a483b20876aea1a42d8d15a023d43d0b32a3
Caesar Wang <wxt@rock-chips.com>
2020-01-15 10:49:42 +0800

    overlay: update npu image for rk3399pro evb v14
    Change-Id: Ia2d9d12af075dd8bde8888a2bc27f507643973e9
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-firmware/usr/bin/npu-image.sh
+++ overlay-firmware/usr/bin/npu-image.sh
@@ -1,12 +1,29 @@
+#!/bin/bash
+
+COMPATIBLE=$(cat /proc/device-tree/compatible)
+
 if [ -e "/sys/devices/platform/f8000000.pcie/pcie_reset_ep" ] ;
 then
-    echo "upgrading npu pcie image......\n"
-    cd /usr/share/npu_fw_pcie/
-    npu_upgrade_pcie MiniLoaderAll.bin uboot.img trust.img boot.img
+    echo "upgrading npu with pcie image......\n"
+
+    if [[ $COMPATIBLE =~ "rk3399pro-evb-v14-linux" ]];
+    then
+        cd /usr/share/npu_fw_pvtm-32k/
+        npu_upgrade_pcie_combine MiniLoaderAll.bin uboot.img trust.img boot.img
+    else
+        cd /usr/share/npu_fw_pcie/
+        npu_upgrade_pcie MiniLoaderAll.bin uboot.img trust.img boot.img
+    fi
 else
-    echo "upgrading npu image......\n"
-    cd /usr/share/npu_fw/
-    npu_upgrade MiniLoaderAll.bin uboot.img trust.img boot.img
+    echo "upgrading npu with usb image......\n"
+    if [[ $COMPATIBLE =~ "rk3399pro-evb-v14-linux" ]];
+    then
+        cd /usr/share/npu_fw_pvtm-32k/
+        npu_upgrade_usb_combine MiniLoaderAll.bin uboot.img trust.img boot.img
+    else
+        cd /usr/share/npu_fw/
+        npu_upgrade MiniLoaderAll.bin uboot.img trust.img boot.img
+    fi
 fi
 
 sleep 1
