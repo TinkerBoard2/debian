64c69e7b90b20ff7e0d9dd1721346072065f3f23
Caesar Wang <wxt@rock-chips.com>
2020-03-05 14:31:55 +0800

    overlay-firmware: update rknpu-fw to v1.3.1
    Change-Id: Ied63b4b3418f562aff0988cc24e3b42438cf6677
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay-firmware/usr/bin/npu-image.sh
+++ overlay-firmware/usr/bin/npu-image.sh
@@ -8,22 +8,18 @@ then
 
     if [[ $COMPATIBLE =~ "rk3399pro-evb-v14-linux" ]];
     then
-        cd /usr/share/npu_fw_pvtm-32k/pcie/
+        cd /usr/share/npu_fw_pcie/
         npu_upgrade_pcie_combine MiniLoaderAll.bin uboot.img trust.img boot.img
+        cd /usr/bin/
+        mv npu_powerctrl_combine npu_powerctrl
     else
         cd /usr/share/npu_fw_pcie/
         npu_upgrade_pcie MiniLoaderAll.bin uboot.img trust.img boot.img
     fi
 else
     echo "upgrading npu with usb image......\n"
-    if [[ $COMPATIBLE =~ "rk3399pro-evb-v14-linux" ]];
-    then
-        cd /usr/share/npu_fw_pvtm-32k/usb/
-        npu_upgrade_usb_combine MiniLoaderAll.bin uboot.img trust.img boot.img
-    else
-        cd /usr/share/npu_fw/
-        npu_upgrade MiniLoaderAll.bin uboot.img trust.img boot.img
-    fi
+    cd /usr/share/npu_fw/
+    npu_upgrade MiniLoaderAll.bin uboot.img trust.img boot.img
 fi
 
 sleep 1
