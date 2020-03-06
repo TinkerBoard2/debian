0b99d996e659b64e4c1525a0ddde4e0902092b1f
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-03-06 12:19:48 +0800

    gst-plugins-bad1.0: Add kmssink
    Major changes:
    1/ Use debian sources
    2/ Add kmssink
    
    https://github.com/JeffyCN/gst-plugins-bad/tree/debian/1.10.4-1
    52d3dc72 d/rules: Skip tests
    c75230fa d: Add gstreamer1.0-kms
    ea768877 New upstream bugfix release
    31f78da4 Merge tag 'upstream/1.10.4'
    9a6b9ee2 New upstream version 1.10.4
    
    Change-Id: I569b0041292291e50ef7a1191b257b3a286fe015
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- mk-rootfs-stretch.sh
+++ mk-rootfs-stretch.sh
@@ -110,6 +110,7 @@ apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-als
 
 dpkg -i  /packages/video/mpp/*
 dpkg -i  /packages/gst-rkmpp/*.deb
+dpkg -i  /packages/gst-bad/*.deb
 apt-get install -f -y
 
 #---------------Others--------------
