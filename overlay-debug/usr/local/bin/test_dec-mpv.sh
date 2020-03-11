b7988ebb30dd628849f1f4af31b87e384007e657
Caesar Wang <wxt@rock-chips.com>
2020-03-11 16:01:51 +0800

    overlay-debug: update scripts for gst and mpv
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I1363ba25a76a40fd5c8929e2bb394d41392beabd

--- overlay-debug/usr/local/bin/test_dec-mpv.sh
+++ overlay-debug/usr/local/bin/test_dec-mpv.sh
@@ -3,6 +3,10 @@ export DISPLAY=:0.0
 #export GST_DEBUG=*:5
 #export GST_DEBUG_FILE=/tmp/2.txt
 
-sudo service lightdm stop
-mpv --hwdec=rkmpp --vo=drm -v --drm-mode=0 -loop /usr/local/test.mp4
-sudo service lightdm start
+su linaro -c "mpv --hwdec=rkmpp --vd-lavc-software-fallback=no --vo=xv /usr/local/test.mp4"
+
+## drm mode
+#sudo service lightdm stop
+#mpv --hwdec=rkmpp --vo=drm -v --drm-mode=0 -loop /usr/local/test.mp4
+#sudo service lightdm start
+
