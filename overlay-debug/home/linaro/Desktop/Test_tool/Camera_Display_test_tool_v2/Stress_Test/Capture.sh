e325a8f78ff8b1eaef06582c6c89f5286b65a17d
xuanhao_zhang <xuanhao_zhang@asus.com>
2020-05-25 09:54:43 +0800

    Camera: update stress test
    Change-Id: Ie8040447683f6e3c5994469ce865120882226180
    
    Former-commit-id: 7141b3d834265e9455426de32deacb0df362725e

--- overlay-debug/home/linaro/Desktop/Test_tool/Camera_Display_test_tool_v2/Stress_Test/Capture.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/Camera_Display_test_tool_v2/Stress_Test/Capture.sh
@@ -4,7 +4,6 @@ i=0
 ERROR=0
 ResultFile="/tmp/Capture_TestResult.txt"
 CSI0="/dev/video0"
-CSI1="/dev/video5"
 output="/tmp"
 
 function Remove_TestResult()
@@ -34,17 +33,36 @@ else
 fi
 
 echo -e "Start Capture Stress Test!" | tee -a $ResultFile
+echo 60 > /tmp/flicker_mode
+cat /sys/bus/i2c/drivers/imx219/1-0010/name |grep "imx219"
+if [ "$?" == "0" ]; then
+	echo -e "Start Preview Test!" | tee -a $ResultFile
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SRGGB10_1X10/3280x2464]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/3280x2464]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/3280x2464]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[crop:(0,0)/3280x2464]'
+	media-ctl -d /dev/media0 --set-v4l2 '"m00_b_imx219 1-0010":0[fmt:SRGGB10_1X10/3280x2464]'
+	v4l2-ctl -d /dev/video0 --set-fmt-video=width=3280,height=2464,pixelformat=NV12 --set-crop=top=0,left=0,width=3280,height=2464
+else
+	echo -e "Start Preview Test!" | tee -a $ResultFile
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SBGGR8_1X8/2592x1944]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/2592x1944]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/2592x1944]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[crop:(0,0)/2592x1944]'
+	media-ctl -d /dev/media0 --set-v4l2 '"m00_b_ov5647 1-0036":0[fmt:SBGGR8_1X8/2592x1944]'
+	v4l2-ctl -d /dev/video0 --set-fmt-video=width=2592,height=1944,pixelformat=NV12 --set-crop=top=0,left=0,width=2592,height=1944
+fi
 
 echo -e "Start Capture Test!" | tee -a $ResultFile
 while [ $i != $count ]; do
     i=$(($i+1))
 
-	gst-launch-1.0 rkv4l2src device=$CSI0 num-buffers=10 ! video/x-raw,format=NV12,width=1920,height=1080, framerate=30/1 ! jpegenc ! multifilesink location=$output/ov5647_$i.jpg &
-	sleep 4
-	
-	gst-launch-1.0 rkv4l2src device=$CSI1 num-buffers=10 ! video/x-raw,format=NV12,width=1920,height=1080, framerate=30/1 ! jpegenc ! multifilesink location=$output/imx219_$i.jpg &
-	sleep 4
-	
+	cat /sys/bus/i2c/drivers/imx219/1-0010/name |grep "imx219"
+	if [ "$?" == "0" ]; then
+		gst-launch-1.0 v4l2src device=$CSI0 num-buffers=10 ! video/x-raw,format=NV12,width=3280,height=2464 ! jpegenc ! multifilesink location=$output/imx219_$i.jpg
+	else
+		gst-launch-1.0 v4l2src device=$CSI0 num-buffers=10 ! video/x-raw,format=NV12,width=2592,height=1944 ! jpegenc ! multifilesink location=$output/ov5647_$i.jpg
+	fi
 	echo -e "$(date): Camera capture $i time(s)" | tee -a $ResultFile
 			
 done
--- overlay-debug/home/linaro/Desktop/Test_tool/Camera_Display_test_tool_v2/Stress_Test/Record.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/Camera_Display_test_tool_v2/Stress_Test/Record.sh
@@ -4,7 +4,6 @@ i=0
 ERROR=0
 ResultFile="/tmp/Record_TestResult.txt"
 CSI0="/dev/video0"
-CSI1="/dev/video5"
 output="/home/linaro/Desktop"
 
 function Remove_TestResult()
@@ -34,29 +33,34 @@ fi
 echo $time
 
 echo -e "Start Record Test!" | tee -a $ResultFile
+echo 60 > /tmp/flicker_mode
+cat /sys/bus/i2c/drivers/imx219/1-0010/name |grep "imx219"
+if [ "$?" == "0" ]; then
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SRGGB10_1X10/1920x1080]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/1920x1080]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/1920x1080]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[crop:(0,0)/1920x1080]'
+	media-ctl -d /dev/media0 --set-v4l2 '"m00_b_imx219 1-0010":0[fmt:SRGGB10_1X10/1920x1080]'
+	v4l2-ctl -d /dev/video0 --set-fmt-video=width=1920,height=1080,pixelformat=NV12 --set-crop=top=0,left=0,width=1920,height=1080
+else
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[fmt:SBGGR8_1X8/1296x972]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":0[crop:(0,0)/1296x972]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[fmt:YUYV8_2X8/1296x972]'
+	media-ctl -d /dev/media0 --set-v4l2 '"rkisp1-isp-subdev":2[crop:(0,0)/1296x972]'
+	media-ctl -d /dev/media0 --set-v4l2 '"m00_b_ov5647 1-0036":0[fmt:SBGGR8_1X8/1296x972]'
+	v4l2-ctl -d /dev/video0 --set-fmt-video=width=1296,height=972,pixelformat=NV12 --set-crop=top=0,left=0,width=1296,height=972
+fi
+
 while [ $i != $time ]; do
 	i=$(($i+1))
-	rm -rf $output/Record_ov5647.avi
-	echo -e "$(date): Start record ov5647 $i time(s)" | tee -a $ResultFile
-	gst-launch-1.0 rkv4l2src device=$CSI0 ! video/x-raw,width=640,height=480,framerate=30/1 ! tee name=t t. ! queue ! kmssink sync=false t. ! queue ! videorate ! video/x-raw,width=640,height=480,framerate=30/1 ! mpph264enc ! queue ! h264parse ! avimux ! filesink location=$output/Record_ov5647.avi &
+	rm -rf $output/Record.avi
+	echo -e "$(date): Start record $i time(s)" | tee -a $ResultFile
+	gst-launch-1.0 v4l2src device=$CSI0 ! video/x-raw,width=640,height=480 ! tee name=t t. ! queue ! autovideosink sync=false t. ! queue ! mpph264enc ! queue ! h264parse ! mpegtsmux ! filesink location=$output/Record.avi &
 	var=$!
 	sleep 3600
 	kill -9 $var
     echo -e "$(date): Camera record $i time(s)" | tee -a $ResultFile
 done
-
-i=0
-while [ $i != $time ]; do
-    i=$(($i+1))
-    rm -rf $output/Record_imx219.avi
-	echo -e "$(date): Start record imx219 $i time(s)" | tee -a $ResultFile
-    gst-launch-1.0 rkv4l2src device=$CSI1 ! video/x-raw,width=640,height=480,framerate=30/1 ! tee name=t t. ! queue ! kmssink sync=false t. ! queue ! videorate ! video/x-raw,width=640,height=480,framerate=30/1 ! mpph264enc ! queue ! h264parse ! avimux ! filesink location=$output/Record_imx219.avi &
-    var=$!
-    sleep 3600
-    kill -9 $var
-    echo -e "$(date): Camera record $i time(s)" | tee -a $ResultFile
-done
-
 
 echo -e "Finished Record Test!" | tee -a $ResultFile
 systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target
