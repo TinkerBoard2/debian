3bfd6918308b31f8660fd584db6a62a015b3474e
Caesar Wang <wxt@rock-chips.com>
2020-06-30 15:46:21 +0800

    overlay: change the rk3288 mali to r18 for rockchip.sh
    Change-Id: Iaa05012985cff6fb815b4a3b820ac955a7494f3c
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- overlay/etc/init.d/rockchip.sh
+++ overlay/etc/init.d/rockchip.sh
@@ -16,9 +16,9 @@ then
     GPU_VERSION=$(cat /sys/devices/platform/*gpu/gpuinfo)
     if echo $GPU_VERSION|grep -q r1p0;
     then
-        dpkg -i  /packages/libmali/libmali-rk-midgard-t76x-r14p0-r1p0_*.deb #3288w
+        dpkg -i  /packages/libmali/libmali-rk-midgard-t76x-r18p0-r1p0_*.deb #3288w
     else
-        dpkg -i  /packages/libmali/libmali-rk-midgard-t76x-r14p0-r0p0_*.deb
+        dpkg -i  /packages/libmali/libmali-rk-midgard-t76x-r18p0-r0p0_*.deb
     fi
     dpkg -i  /packages/libmali/libmali-rk-dev_*.deb
 elif [[  "$1" == "rk3328"  ]]; then
