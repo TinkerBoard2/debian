b9401dbe91b4c66f7ab1b920eb8557b3a6c00330
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-07-14 15:19:47 +0800

    mpp: Update packages
    23e5a7bf debian: Add demo packages
    a53dd938 debian: Remove static libs
    49ec1f4e debian/rules: Only use dpkg-cross when cross compiling
    337e917d [h264e_slice]: Fix slice amend and prefix nal
    f654f1a0 [rk_venc_cmd]: Change h264 codec cfg
    7f4ebeec [meta]: Add more meta key for encoder
    4397cb30 [astyle]: Do NOT change drm.h coding style
    d710e261 [h264e_slice]: Fix slice update error
    
    NOTE:
    "a57fd68c [vdpu2]: fix bug: set ref list" been reverted in armhf build
    due to a known issue.
    
    Change-Id: Ib5b4322bdd281465592e8ffcd5190dea0e2c94cc
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- mk-rootfs-stretch.sh
+++ mk-rootfs-stretch.sh
@@ -108,7 +108,7 @@ cp /etc/Powermanager/triggerhappy.service  /lib/systemd/system/triggerhappy.serv
 echo -e "\033[36m Setup Video.................... \033[0m"
 apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base-apps
 
-dpkg -i  /packages/video/mpp/*
+dpkg -i  /packages/mpp/*
 dpkg -i  /packages/gst-rkmpp/*.deb
 dpkg -i  /packages/gst-base/*.deb
 apt-mark hold gstreamer1.0-x
