e070efdc22119579ff695e8af9d01f3d20cc378f
Rock Shen <rock_shen@asus.com>
2021-01-26 15:29:21 +0800

    Fix Blueman Devices option no response for buster
    Change-Id: Ide8acd3542f4903a53f18cb2dbd979091f0fe5f4
    
    Former-commit-id: c7553e1cb83f424700a0b4c1df30061675644645

--- mk-rootfs-buster.sh
+++ mk-rootfs-buster.sh
@@ -193,6 +193,10 @@ systemctl mask systemd-networkd-wait-online.service
 systemctl mask NetworkManager-wait-online.service
 rm /lib/systemd/system/wpa_supplicant@.service
 
+#-------------blueman--------------
+bash /etc/init.d/blueman.sh
+rm /etc/init.d/blueman.sh
+
 echo $VERSION_NUMBER-$VERSION > /etc/version
 
 #---------------Clean--------------
