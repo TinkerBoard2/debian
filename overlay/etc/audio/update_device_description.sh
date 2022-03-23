c64520dbcf10e8fc7791befac5901e7c6217fd0f
yupei_lin <yupei_lin@asus.com>
2022-03-23 14:41:09 +0800

    Audio : add script to update sound card description for hdmi and dp
    Change-Id: If2642d498a2e5259b2ed2eda5bbe717b639c98ee
    
    Former-commit-id: 7fb5831f3e817f7af352dcbcf0264d99a478f544

--- overlay/etc/audio/update_device_description.sh
+++ overlay/etc/audio/update_device_description.sh
@@ -2,8 +2,8 @@
 
 echo "Update device.description for pulseaudio 12.2"
 sleep 5
-sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd update-sink-proplist alsa_output.platform-hdmi-dp-sound.stereo-fallback device.description="HDMI_DP-Sound-Output"
-sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd update-sink-proplist alsa_output.platform-spdif-sound.stereo-fallback device.description="SPDIF-Sound-Output"
+sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd update-sink-proplist alsa_output.platform-dp-sound.stereo-fallback device.description="DP_SPDIF-Sound-Output"
+sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd update-sink-proplist alsa_output.platform-hdmi-sound.stereo-fallback device.description="HDMI-Sound-Output"
 sound_ext_card_name=`sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd list-cards | grep -A 10 alsa_card.platform-sound-ext-card | grep alsa.card_name`
 sound_ext_alsa_card_name=$(echo $sound_ext_card_name | cut -d" " -f 3)
 sudo -u linaro PULSE_RUNTIME_PATH=/run/user/1000/pulse pacmd update-sink-proplist alsa_output.platform-sound-ext-card.stereo-fallback device.description=$sound_ext_alsa_card_name
